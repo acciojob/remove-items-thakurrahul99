bc095436a7e7c3421f3f998cc03d83657caf479d
Rahul Singh <rahulsinghrajpoot9997@gmail.com>
2025-10-07 21:52:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+ window.onload = function () {
+    const select = document.getElementById('colorSelect');
+    const button = document.querySelector('input[type="button"][value="Select and Remove"]');
+
+    button.onclick = function () {
+      const selectedIndex = select.selectedIndex;
+      if (selectedIndex !== -1) {
+        select.remove(selectedIndex);
+      }
+    };
+  };
